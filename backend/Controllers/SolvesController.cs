65e609ab0b6486df3626f027fa75d1ecb244a157
danny tran <tdan7735@gmail.com>
2026-09-01 01:35:24 +1000

    added solves object with a simple get request which returns temporary placeholder values

---
+++ backend/Controllers/SolvesController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class SolvesController : ControllerBase {
+    [HttpGet]
+    public IActionResult GetSolves() {
+        return Ok(new[]
+                {
+                    "12.42",
+                    "11.83",
+                    "67",
+                });
+    }
+}
